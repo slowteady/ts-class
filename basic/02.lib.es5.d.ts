68661bcef23cff01a746e95e503cddf483ae24c3
slowteady <vbn0213@gmail.com>
2023-08-09 23:08:09 +0900

    feat: filter 함수 분석 및 예제 추가

--- basic/02.lib.es5.d.ts
+++ basic/02.lib.es5.d.ts
@@ -1,5 +1,5 @@
-// 01. forEach. map 제네릭 분석
 interface Array<T> {
+  // 01. forEach. map 제네릭 분석
   forEach(
     callbackfn: (value: T, index: number, array: T[]) => void,
     thisArg?: any
@@ -9,5 +9,23 @@ interface Array<T> {
   map<U>(
     callbackfn: (value: T, index: number, array: T[]) => U,
     thisArg?: any
-  ): U[]; 
+  ): U[];
+
+  // 02. filter 제네릭 분석
+  filter<S extends T>(
+    predicate: (value: T, index: number, array: T[]) => value is S,
+    thisArg?: any
+  ): S[];
+  filter(
+    predicate: (value: T, index: number, array: T[]) => unknown,
+    thisArg?: any
+  ): T[];
 }
+
+// 02.
+// const filtered = ['1', 2, '3', 4, '5'].filter((value) => typeof value === 'string');
+// => const filtered: (string | number)[] 로 추론 정상적으로 하지 못함
+// value is string 을 통해 S가 string 값을 알려서 사용할 수 있음
+const predicate = (value: string | number): value is string =>
+  typeof value === "string";
+const filtered = ["1", 2, "3", 4, "5"].filter(predicate);
