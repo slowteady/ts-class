d6d72326a5ddb8c0a902be5c7b8bec8d7b18aeab
slowteady <vbn0213@gmail.com>
2023-08-10 15:29:08 +0900

    feat: 파일명 변경, 함수 직접 만드는 예제 추가

--- basic/02.lib.es5.ts
+++ basic/02.lib.es5.ts
@@ -29,3 +29,25 @@ interface Array<T> {
 const predicate = (value: string | number): value is string =>
   typeof value === "string";
 const filtered = ["1", 2, "3", 4, "5"].filter(predicate);
+
+// 03. forEach 직접 만들기
+interface Arr<T> {
+  forEach(callback: (value: T, index: number) => void): void;
+}
+const a1: Arr<number> = [1, 2, 3];
+a1.forEach((item) => item);
+
+// 04. map 직접 만들기
+// T에 하나의 타입이 들어갈 수 있는 것이 아니기 때문에 U로 따로 뺌
+interface Arr2<T> {
+  map<U>(callback: (value: T) => U): U[];
+}
+const a2: Arr2<number> = [1, 2, 3];
+a2.map((arr) => arr * 2);
+
+// 05. filter 직접 만들기
+interface Arr3<T> {
+  filter<U extends T>(callback: (value: T) => value is U): U[];
+}
+const a3: Arr3<number | string> = ["1", 2, 3, "4"];
+a3.filter((arr): arr is string => typeof arr === "string");
